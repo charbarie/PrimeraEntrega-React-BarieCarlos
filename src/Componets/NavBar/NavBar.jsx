2cd8dea2983b8cd58a4571dd815da068e4bb150d
Charly Barié <@char.barie@gmail.com>
2023-10-30 21:55:40 -0300

    fix

--- src/Componets/NavBar/NavBar.jsx
+++ src/Componets/NavBar/NavBar.jsx
@@ -2,13 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
- 
+import './NavBar.css'
 export const NavBar = () => {
   return (
 
-<Navbar expand="lg" className="">
+<Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home"><E-commerce></E-commerce></Navbar.Brand>
+        <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
